613a0314eb27573e80e36a4133a05cddefafbac9
Mustafa Arınmış <56651041+Arinmis@users.noreply.github.com>
2021-05-23 12:53:47 +0300

    Create Abbreviation.java

---
+++ Abbreviation.java
@@ -0,0 +1,22 @@
+import java.util.*;
+ 
+public class Abbreviation { 
+ 
+    public static void main(String[] args) { 
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for(int i=0; i<n; i++) { 
+            String word = input.next();
+            word = abbrevate(word);
+            System.out.println(word);
+        }
+    }
+ 
+    public static String abbrevate(String str) { 
+        if(str.length() <= 10)
+            return str;
+        return str.substring(0, 1) + str.substring(1, str.length() - 1).length() + 
+            str.substring(str.length() - 1);
+    }
+ 
+}
