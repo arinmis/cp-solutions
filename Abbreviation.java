11c2605cbcd6e1158b936bc3410d4c8e91a49837
Mustafa Arınmış <56651041+Arinmis@users.noreply.github.com>
2021-05-23 16:27:32 +0300

    Update Abbreviation.java

--- Abbreviation.java
+++ Abbreviation.java
@@ -7,16 +7,16 @@ public class Abbreviation {
         int n = input.nextInt();
         for(int i=0; i<n; i++) { 
             String word = input.next();
-            word = abbrevate(word);
+            word = abbreviate(word);
             System.out.println(word);
         }
     }
  
-    public static String abbrevate(String str) { 
+ 
+    public static String abbreviate(String str) { 
         if(str.length() <= 10)
             return str;
-        return str.substring(0, 1) + str.substring(1, str.length() - 1).length() + 
-            str.substring(str.length() - 1);
+        return str.substring(0, 1) + str.substring(1, str.length() - 1).length() +              str.substring(str.length() - 1);
     }
  
 }
